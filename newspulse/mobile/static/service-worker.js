f55c73c371fc991b811116ed85c8bed0bcf740ac
kbdsky <kbdsky@gmail.com>
2026-09-04 00:32:24 +0900

    ⚡ Add newspulse/mobile/static/service-worker.js

---
+++ newspulse/mobile/static/service-worker.js
@@ -0,0 +1,52 @@
+// Service Worker for ChargersPulse PWA
+const CACHE_NAME = 'chargers-pulse-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/index.html',
+  '/static/manifest.json',
+  '/api/briefing'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network first, falling back to cache
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone response and cache it
+        if (event.request.method === 'GET') {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, resClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
