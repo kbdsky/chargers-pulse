f21583fd061a32c76a201ea4f11b7718009f1989
kbdsky <kbdsky@gmail.com>
2026-09-04 00:31:30 +0900

    ⚡ Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from newspulse.mobile.server import app
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
